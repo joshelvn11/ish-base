287b929c3bd7383896a388bd499e3d3cd3a6e69f
Josh Beaver <josh.elvn11@gmail.com>
2024-06-13 23:57:34 +0100

    [FEAT] added user detail view

--- users/views.py
+++ users/views.py
@@ -44,3 +44,22 @@ class Login(APIView):
         }
 
         return respone
+
+
+class UserView(APIView):
+
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Unauthenticated request')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated request')
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
