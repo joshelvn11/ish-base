9d1c2246c526cdcfeeed252bf61aaea02fffbd0d
Josh Beaver <josh.elvn11@gmail.com>
2024-06-20 18:16:31 +0100

    [FIX] added root url handling

--- ishbase/urls.py
+++ ishbase/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Hello, world!")
+
 
 urlpatterns = [
    path('admin/', admin.site.urls),
-   path('api/', include('users.api.urls'))
+   path('api/', include('users.api.urls')),
+   path('', home),
 ]
