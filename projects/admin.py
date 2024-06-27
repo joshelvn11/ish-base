876a20374288b1856d9eba350eec8d5c562a64ed
Josh Beaver <josh.elvn11@gmail.com>
2024-06-27 12:44:33 +0100

    [MISC] registerd models to admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Project, Epic, Sprint, UserStory, Task
 
-# Register your models here.
+admin.site.register(Project)
+admin.site.register(Epic)
+admin.site.register(Sprint)
+admin.site.register(UserStory)
+admin.site.register(Task)
