95707e910fb64f0ea595f7134fa0e4048f0b3f37
Josh Beaver <josh.elvn11@gmail.com>
2024-06-28 21:40:34 +0100

    [MISC] added epic and user story urls

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProjectListCreateAPIView, ProjectDetailAPIView
+from .views import ProjectListCreateAPIView, ProjectDetailAPIView, EpicListCreateAPIView, EpicDetailAPIView, UserStoryListCreateAPIView, UserStoryDetailAPIView
 
 urlpatterns = [
     path('projects/', ProjectListCreateAPIView.as_view(), name='project-list-create'),
     path('projects/<int:pk>/', ProjectDetailAPIView.as_view(), name='project-detail'),
+    path('projects/<int:pk>/epics/', EpicListCreateAPIView.as_view(), name='epic-list-create'),
+    path('projects/<int:pk>/user-stories/', UserStoryListCreateAPIView.as_view(), name='user-story-list-create'),
 ]
