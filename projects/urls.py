c880c6f5d6a95be058026ee6c944c49346ef451e
Josh Beaver <josh.elvn11@gmail.com>
2024-06-28 21:46:09 +0100

    [MISC] added task list create view to urls

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProjectListCreateAPIView, ProjectDetailAPIView, EpicListCreateAPIView, EpicDetailAPIView, UserStoryListCreateAPIView, UserStoryDetailAPIView
+from .views import ProjectListCreateAPIView, ProjectDetailAPIView, EpicListCreateAPIView, EpicDetailAPIView, UserStoryListCreateAPIView, UserStoryDetailAPIView, TaskListCreateAPIView
 
 urlpatterns = [
     path('projects/', ProjectListCreateAPIView.as_view(), name='project-list-create'),
     path('projects/<int:pk>/', ProjectDetailAPIView.as_view(), name='project-detail'),
     path('projects/<int:pk>/epics/', EpicListCreateAPIView.as_view(), name='epic-list-create'),
     path('projects/<int:pk>/user-stories/', UserStoryListCreateAPIView.as_view(), name='user-story-list-create'),
+    path('projects/<int:pk>/tasks/', TaskListCreateAPIView.as_view(), name='task-list-create'),
 ]
