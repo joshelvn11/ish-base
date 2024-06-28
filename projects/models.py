87cd8b9ea72bbbc938ee1ea64f1f562e813cea82
Josh Beaver <josh.elvn11@gmail.com>
2024-06-28 21:39:30 +0100

    [IMPROV] added status choice, allowed blank values in models

--- projects/models.py
+++ projects/models.py
@@ -4,7 +4,12 @@ from django.contrib.auth.models import User
 PRIORITY = [(1, "CRITICAL"),
             (2, "ESSENTIAL"),
             (3, "BENEFICIAL"),
-            (4, ("OPTIONAL"))]
+            (4, "OPTIONAL")]
+
+STATUS = [("TO DO", "TO DO"),
+          ("IN PROGRESS", "IN PROGRESS"),
+          ("REVIEW", "REVIEW"),
+          ("DONE", "DONE")]
 
 
 class Project(models.Model):
@@ -20,6 +25,7 @@ class Epic(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     name = models.CharField(max_length=256)
     description = models.TextField(null=True)
+    status = models.CharField(choices=STATUS, default="TO DO")
 
     def __str__(self):
         return f"[EPIC] {self.name} [PROJECT] {self.project.name} [USER] {self.project.owner.username}"
@@ -36,21 +42,23 @@ class Sprint(models.Model):
 class UserStory(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     epic = models.ForeignKey(Epic, on_delete=models.CASCADE, null=True)
-    sprint = models.ForeignKey(Sprint, on_delete=models.SET_NULL, null=True)
+    sprint = models.ForeignKey(Sprint, on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField(max_length=256)
-    description = models.TextField(null=True)
-    user_story = models.TextField(null=True)
-    subtasks = models.JSONField(null=True, editable=True)
-    due_date = models.DateField(null=True)
-    priority = models.IntegerField(choices=PRIORITY, null=True)
+    description = models.TextField(null=True, blank=True)
+    user_story = models.TextField(null=True, blank=True)
+    subtasks = models.JSONField(null=True, editable=True, blank=True)
+    due_date = models.DateField(null=True, blank=True)
+    priority = models.IntegerField(choices=PRIORITY, null=True, blank=True)
+    status = models.CharField(choices=STATUS, default="TO DO")
 
 
 class Task(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    epic = models.ForeignKey(Epic, on_delete=models.CASCADE, null=True)
-    sprint = models.ForeignKey(Sprint, on_delete=models.SET_NULL, null=True)
+    epic = models.ForeignKey(Epic, on_delete=models.CASCADE, null=True, blank=True)
+    sprint = models.ForeignKey(Sprint, on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField(max_length=256)
-    description = models.TextField(null=True)
-    subtasks = models.JSONField(null=True, editable=True)
-    due_date = models.DateField(null=True)
-    priority = models.IntegerField(choices=PRIORITY, null=True)
+    description = models.TextField(null=True, blank=True)
+    subtasks = models.JSONField(null=True, editable=True, blank=True)
+    due_date = models.DateField(null=True, blank=True)
+    priority = models.IntegerField(choices=PRIORITY, null=True, blank=True)
+    status = models.CharField(choices=STATUS, default="TO DO")
